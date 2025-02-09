4a3603244c4ad521be8138ebb63586da2f9728cf
Gabriel <correo@correo.com>
2025-02-09 18:06:50 -0400

    Se pueden agregar categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,12 +4,10 @@ import { AgregarCategoria } from "./components/AgregarCategoria";
 export function GifExpertApp(){
   const [ categorias, setCategorias ] = useState(["One Punch","Dragon ball"])
 
-  const onAgregarCategoria = () => setCategorias([...categorias, "Valorant"]);
   return(
     <>
       <h1>GifExpertApp</h1>
-      <AgregarCategoria />
-      <button onClick={ onAgregarCategoria } >Agregar</button>
+      <AgregarCategoria setCategorias={setCategorias} />
       <ol>
         { categorias.map( c => ( <li key={c}>{ c }</li> ) )}
       </ol>
--- src/components/AgregarCategoria.jsx
+++ src/components/AgregarCategoria.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
-export const AgregarCategoria = () => {
+export const AgregarCategoria = ({ setCategorias }) => {
   const [input, setInput] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim().length <= 1 ) return;
+    setCategorias(c => [input, ...c]);
+  }
+
 
   return(
-    <form>
+    <form onSubmit={ handleSubmit }>
       <input 
         type="text"
         placeholder="Ingresa una categoría"
